beed5cabcc35a933d94a749964dafcbda84dfd1b
Rustam Agzamov <rustam@mysuperdispatch.com>
2020-06-20 00:00:16 +0500

    Add basic search input

---
+++ src/cards/SearchInput.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
+import InputBase from "@material-ui/core/InputBase";
+import IconButton from "@material-ui/core/IconButton";
+import SearchIcon from "@material-ui/icons/Search";
+import Card from "@material-ui/core/Card";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      padding: "2px 0",
+      display: "flex",
+      alignItems: "center",
+      maxWidth: 345,
+    },
+    input: {
+      marginLeft: theme.spacing(1),
+      flex: 1,
+    },
+    iconButton: {
+      padding: 10,
+    },
+  })
+);
+
+export default function CustomizedInputBase() {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root} variant="outlined">
+      <InputBase className={classes.input} placeholder="Search Cards" />
+      <IconButton className={classes.iconButton} aria-label="search">
+        <SearchIcon />
+      </IconButton>
+    </Card>
+  );
+}
