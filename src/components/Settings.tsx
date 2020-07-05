ec73d6118e6c6447029609938d7ea371f10046ea
Rustam Agzamov <rustam@mysuperdispatch.com>
2020-07-05 23:48:42 +0500

    Make card settings collapsable

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,11 +1,13 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import { Field, FieldConfig } from "components/FieldComponent";
 import { textFieldSettings } from "components/TextField/TextFieldSettings";
-import { Grid, Box, Typography } from "@material-ui/core";
+import { Grid, Box, Typography, Collapse } from "@material-ui/core";
 import { CardConfig } from "cards/demo/DemoCards";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import ExpandLessIcon from "@material-ui/icons/ExpandLess";
 
 const useStyles = makeStyles({
   root: {
@@ -32,23 +34,31 @@ interface FieldSettingsProps {
   field: FieldConfig;
 }
 
-const FieldSettings = ({ classes, field }: FieldSettingsProps) =>
-  field.name === "textfield" ? (
+const FieldSettings = ({ classes, field }: FieldSettingsProps) => {
+  const [isExpanded, setIsExpanded] = useState(true);
+  const handleClick = () => setIsExpanded(!isExpanded);
+  return field.name === "textfield" ? (
     <Card variant="outlined" className={classes.root}>
       <CardContent>
-        <Typography color="textPrimary" variant="h6" gutterBottom={true}>
-          {field.props.label || field.props.placeholder}
-        </Typography>
-        <Grid container spacing={2} direction="column">
-          {textFieldSettings(field.props).map((field, index) => (
-            <Grid key={index} item>
-              <Field {...field} />
-            </Grid>
-          ))}
-        </Grid>
+        <Box display="flex" alignItems="center" onClick={handleClick}>
+          <Typography color="textPrimary" variant="h6" style={{ flexGrow: 1 }}>
+            {field.props.label || field.props.placeholder}
+          </Typography>
+          {isExpanded ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+        </Box>
+        <Collapse in={isExpanded} timeout="auto" unmountOnExit>
+          <Grid container spacing={2} direction="column">
+            {textFieldSettings(field.props).map((field, index) => (
+              <Grid key={index} item>
+                <Field {...field} />
+              </Grid>
+            ))}
+          </Grid>
+        </Collapse>
       </CardContent>
     </Card>
   ) : null;
+};
 
 export const Settings: FunctionComponent<SettingsProps> = (props) => {
   const classes = useStyles();
