70eabc1bfd1d26989d16eb0a13ea45a431348266
Rustam Agzamov <rustam@mysuperdispatch.com>
2020-07-14 00:05:48 +0500

    Remove unused interface

--- src/DropPlaceholder.tsx
+++ src/DropPlaceholder.tsx
@@ -1,14 +1,11 @@
 import React, { FunctionComponent } from "react";
-import styled, { CSSProperties } from "styled-components";
+import styled from "styled-components";
 import { useSelector } from "react-redux";
 import { Store } from "./redux/store";
 
 const StyledDropPlaceholder = styled.div`
   box-sizing: border-box;
 `;
-interface DropPlaceHolderProps {
-  style?: CSSProperties;
-}
 export const DropPlaceHolder: FunctionComponent = () => {
   const placeholderOrder = useSelector(
     ({ draggables }: Store) => draggables.placeholderOrder
